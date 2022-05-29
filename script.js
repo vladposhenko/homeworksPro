58b36da3ef028e34d7c95ad3d5004ac74d38f1c2
vladposhenko <98976571+vladposhenko@users.noreply.github.com>
2022-05-29 14:43:30 +0300

    123

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('**************');
+
+PiNumber = Math.PI;
+
+SquareNumber = 314;
+
+RadiusNumber = 10;
+
+HeightNumber = 20;
+
+
+console.log("Обьем цилиндра с площадью основы S = ", SquareNumber , "радиусом R = ", RadiusNumber, "и высотой H =", HeightNumber);
+
+console.log('--------------------');
+
+console.log(`V =`, (PiNumber*(RadiusNumber*RadiusNumber)*HeightNumber));
+
+console.log('--------------------');
+
+console.log('end');
