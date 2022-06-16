b1decdfee99a616e5c9fa74ff0297d0aa07e08ae
vladposhenko <98976571+vladposhenko@users.noreply.github.com>
2022-06-16 16:44:38 +0300

    1232131212

--- script.js
+++ script.js
@@ -19,30 +19,30 @@ summer_sports = sports.slice(5,11);
 fruits = winter_sports.splice(2,1);
 fruits = fruits.concat(summer_sports.splice(2,2)) 
 
-
+console.log('');
 console.log('***WINTER SPORTS***');
 for(i = 0; i < winter_sports.length; i++){
     for(j = 0; j < winter_sports[i].length; j++){
-        console.log(`${winter_sports[i][j+1]}: ${winter_sports[i][j]}`);
+        console.log(`${winter_sports[i][j]}: ${winter_sports[i][j+1]}`);
         break;
     }
 }
 
-
+console.log('');
 console.log('***SUMMER SPORTS***');
 for(i = 0; i < summer_sports.length; i++){
     for(j = 0; j < summer_sports[i].length; j++){
-        console.log(`${summer_sports[i][j+1]}:  ${summer_sports[i][j]}`);
+        console.log(`${summer_sports[i][j]}:  ${summer_sports[i][j+1]}`);
         break;
     }
 }
 
 
-
+console.log('');
 console.log('***FRUITS***');
 for(i = 0; i < fruits.length; i++){
     for(j = 0; j < fruits[i].length; j++){
-        console.log(`${fruits[i][j+1]}:  ${fruits[i][j]}`);
+        console.log(`${fruits[i][j]}:  ${fruits[i][j+1]}`);
         break;
     }
 }
