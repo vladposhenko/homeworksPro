d06d3259bfea1e4d6dcf99113ca91222a2e664b8
vladposhenko <98976571+vladposhenko@users.noreply.github.com>
2022-07-05 19:30:50 +0300

    upd

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const data1 = {
-    k: 2,
-    b: 20,
-    c: 30,
-}
-
-const data2 = {
-    m: 2,
-    b: 10,
-    c: 52,
-}
-
 let assignObjects = (obj1, obj2) =>  Object.assign(obj1,obj2);
 
-console.log(assignObjects(data1,data2));
+console.log(assignObjects({ x: 10 }, { x: 20, y: 30 }));
