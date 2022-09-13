c4a52cbb71353b716242c05db359b9d2784c6efe
vladposhenko <98976571+vladposhenko@users.noreply.github.com>
2022-09-13 13:32:00 +0300

    commit

---
+++ calculator/calculator.js
@@ -0,0 +1,7 @@
+const add = require('./add.js');
+const sub = require('./sub.js');
+const mult = require('./mult.js');
+const div = require('./div.js');
+const calculator = { add, sub, mult, div };
+
+module.exports = calculator;
